f08f5a718d61e9e72a91e59212295e97118a9faa
Leonamin <yism810@gmail.com>
2024-12-23 17:20:52 +0900

    # ADD - cordova용 run.sh 추가

---
+++ cordova-app/run.sh
@@ -0,0 +1,17 @@
+#! /bin/zsh
+
+if [ $# -eq 0 ]; then
+    echo "인자를 입력해주세요. (0: Android, 1: iOS)"
+    exit 1
+fi
+
+if [ $1 -eq 0 ]; then
+    cordova run android
+elif [ $1 -eq 1 ]; then
+    ps -ef | grep -i simulator | awk '{print $2}' | xargs kill -9 2>/dev/null || true
+
+    cordova run ios --target="iPhone-16-Pro"
+else
+    echo "올바른 인자를 입력해주세요. (0: Android, 1: iOS)"
+    exit 1
+fi
