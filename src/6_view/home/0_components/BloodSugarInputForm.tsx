9d8c2b62d48f8b30ee938ea96991551beb375c9a
Leonamin <yism810@gmail.com>
2024-12-24 17:31:32 +0900

    # ADD - 커서 색상 수정

--- src/6_view/home/0_components/BloodSugarInputForm.tsx
+++ src/6_view/home/0_components/BloodSugarInputForm.tsx
@@ -28,10 +28,10 @@ export default function BloodSugarInputForm({
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = e.target.value;
-    
+
     // 숫자와 소수점만 허용
     if (!/^\d*\.?\d{0,1}$/.test(newValue)) return;
-    
+
     // 입력값이 비어있거나 '.' 로 시작하는 경우 '0' 또는 '0.'으로 설정
     if (newValue === '' || newValue === '.') {
       setLocalValue('0');
@@ -43,7 +43,7 @@ export default function BloodSugarInputForm({
     const formattedValue = newValue.includes('.')
       ? newValue
       : newValue.replace(/^0+(\d)/, '$1');
-    
+
     setLocalValue(formattedValue);
     onChange(formattedValue);
   };
@@ -60,7 +60,7 @@ export default function BloodSugarInputForm({
   return (
     <div className="relative flex flex-col items-center">
       <SemiCircleStepIndicator step={step} />
-      
+
       <div className="absolute bottom-0 flex items-center gap-1">
         <input
           type="number"
@@ -69,9 +69,9 @@ export default function BloodSugarInputForm({
           onChange={handleChange}
           maxLength={5}
           className={twMerge(
-            "w-16 bg-transparent text-center outline-none caret-bg-brand",
+            "w-16 bg-transparent text-center outline-none",
             "text-h3b text-right color-text-primary",
-            "[caret-width:3px]"
+            "caret-indigo-500 dark:caret-indigo-300",
           )}
         />
         <span className="text-body-1m color-text-tertiary">
