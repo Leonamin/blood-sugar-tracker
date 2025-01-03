93f198ccdbf926f45a7ac703224e4f6e1ba2d41d
Leonamin <yism810@gmail.com>
2025-01-03 15:04:16 +0900

    # ADD - 앱바 무조건적으로 타이틀을 가운데로 위치

--- src/1_components/ui/layout/appBar.tsx
+++ src/1_components/ui/layout/appBar.tsx
@@ -5,25 +5,34 @@ import { useNavigate } from "react-router-dom";
 interface AppBarProps {
   title: string;
   className?: string;
+  actions?: React.ReactNode;
 }
 
-const AppBar = ({ title, className }: AppBarProps) => {
+const AppBar = ({ title, className, actions }: AppBarProps) => {
   const navigate = useNavigate();
 
   return (
-    <div className={cn("flex justify-between items-center space-x-4 min-h-[56px]", 
+    <div className={cn("flex items-center min-h-[56px] relative",
       "absolute top-0 left-0 right-0",
-    className)}>
-      <button
-        className="p-4"
-        onClick={() => navigate(-1)}>
-        <IconChevronLeft size={24} />
-      </button>
-      <div className="flex-1">
-        <h1 className="text-body1sb text-center">{title}</h1>
+      className)}>
+      {/* back button container */}
+      <div className="absolute left-0">
+        <button
+          className="p-4"
+          onClick={() => navigate(-1)}>
+          <IconChevronLeft size={24} />
+        </button>
       </div>
-      {/* 56px placeholder */}
-      <div className="w-[56px]"></div>
+
+      {/* title - centered regardless of other elements */}
+      <div className="flex-1 flex justify-center">
+        <h1 className="text-body1sb">{title}</h1>
+      </div>
+
+      {/* actions container */}
+      <div className="absolute right-0">
+        {actions}
+      </div>
     </div>
   )
 }
