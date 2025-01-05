9aacff8599a90a93065b51a4c684a3bb1bcb7230
Leonamin <yism810@gmail.com>
2025-01-05 15:38:15 +0900

    # DELETE - namespace

--- src/7_utils/utils.ts
+++ src/7_utils/utils.ts
@@ -1,65 +1,63 @@
-namespace Utils {
-  export function isZero(value: number): boolean {
+export const Utils = {
+  isZero(value: number): boolean {
     return value === 0;
-  }
+  },
 
-  export function isNaN(value: number): boolean {
+  isNaN(value: number): boolean {
     return Number.isNaN(value);
-  }
+  },
 
-  export function isNotNaN(value: number): boolean {
+  isNotNaN(value: number): boolean {
     return !isNaN(value);
-  }
+  },
 
-  export function isNumber(value: number): boolean {
-    return typeof value === 'number';
-  }
+  isNumber(value: number): boolean {
+    return typeof value === "number";
+  },
 
-  export function isString(value: string): boolean {
-    return typeof value === 'string';
-  }
+  isString(value: string): boolean {
+    return typeof value === "string";
+  },
 
-  export function isBoolean(value: boolean): boolean {
-    return typeof value === 'boolean';
-  }
+  isBoolean(value: boolean): boolean {
+    return typeof value === "boolean";
+  },
 
-  export function isUndefined(value: undefined): boolean {
-    return typeof value === 'undefined';
-  }
+  isUndefined(value: undefined): boolean {
+    return typeof value === "undefined";
+  },
 
-  export function isNull(value: null): boolean {
+  isNull(value: null): boolean {
     return value === null;
-  }
+  },
 
-  export function isNullOrUndefined(value: null | undefined): boolean {
+  isNullOrUndefined(value: null | undefined): boolean {
     return value === null || value === undefined;
-  }
+  },
 
-  export function isEmpty(value: string): boolean {
+  isEmpty(value: string): boolean {
     return value.length === 0;
-  }
+  },
 
-  export function isNotEmpty(value: string): boolean {
-    return !isEmpty(value);
-  }
+  isNotEmpty(value: string): boolean {
+    return !Utils.isEmpty(value);
+  },
 
-  export function isNullOrEmpty(value: null | undefined | string): boolean {
-    return value === null || value === undefined || value === '';
-  }
+  isNullOrEmpty(value: null | undefined | string): boolean {
+    return value === null || value === undefined || value === "";
+  },
 
-  export function isNotNullOrEmpty(value: null | undefined | string): boolean {
-    return !isNullOrEmpty(value);
-  }
+  isNotNullOrEmpty(value: null | undefined | string): boolean {
+    return !Utils.isNullOrEmpty(value);
+  },
 
-  export function parseSafeInt(value: string): number {
+  parseSafeInt(value: string): number {
     const parsedValue = parseInt(value);
     return isNaN(parsedValue) ? 0 : parsedValue;
-  }
+  },
 
-  export function parseSafeFloat(value: string): number {
+  parseSafeFloat(value: string): number {
     const parsedValue = parseFloat(value);
     return isNaN(parsedValue) ? 0 : parsedValue;
-  }
-}
-
-export { Utils };
+  },
+};
